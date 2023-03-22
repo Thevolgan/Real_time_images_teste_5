03e32d4934b5877b1d903afd4439ee1d4881ba53
Franklin <fvolgan@gmail.com>
2023-03-22 11:40:53 -0400

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'images_real_time_test4';
+}
